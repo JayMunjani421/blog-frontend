d59a2a3bcd0b3b9cadc7adf7706ac08b177b2fc9
jaymunjani <161270692+JayMunjani421@users.noreply.github.com>
2025-07-30 08:45:31 +0530

    Update constant.js

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,1 +1,3 @@
-export const BASE_URL  = "http://localhost:8000/api/";
+// export const BASE_URL  = "http://localhost:8000/api/";
+
+export const BASE_URL  = "https://blog-backend-wy92.onrender.com/api/";
